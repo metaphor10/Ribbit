ea17a7d94ada5349dab9cd54c3c109bc2618b679
Ariel <borochov@american.edu>
2015-01-06 08:18:21 -0500

    adding firends

--- app/src/main/java/com/software/arielb/ribbit/EditFriendsActivity.java
+++ app/src/main/java/com/software/arielb/ribbit/EditFriendsActivity.java
@@ -1,11 +1,14 @@
 package com.software.arielb.ribbit;
 
 import android.app.AlertDialog;
+import android.app.ListActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ArrayAdapter;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -15,25 +18,38 @@ import com.parse.ParseUser;
 import java.util.List;
 
 
-public class EditFriendsActivity extends ActionBarActivity {
+public class EditFriendsActivity extends ListActivity {
     public static final String TAG=EditFriendsActivity.class.getSimpleName();
+    protected List<ParseUser> mUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_edit_friends);
     }
     @Override
     protected void onResume(){
         super.onResume();
+        setProgressBarIndeterminateVisibility(true);
         ParseQuery<ParseUser> query= ParseUser.getQuery();
         query.orderByAscending(ParseConstants.KEY_USERNAME);
         query.setLimit(1000);
         query.findInBackground(new FindCallback<ParseUser>() {
             @Override
-            public void done(List<ParseUser> parseUsers, ParseException e) {
+            public void done(List<ParseUser> users, ParseException e) {
+                setProgressBarIndeterminateVisibility(false);
                 if(e==null){
                     //success
+                    mUsers=users;
+                    String [] usernames=new String[mUsers.size()];
+                    int i=0;
+                    for (ParseUser user: mUsers){
+                        usernames[i]=user.getUsername();
+                        i++;
+                    }
+                    ArrayAdapter<String> adapter=new ArrayAdapter<String>(EditFriendsActivity.this,android.R.layout.simple_list_item_checked,usernames);
+                    setListAdapter(adapter);
                 }else {
                     Log.e(TAG, e.getMessage());
                     AlertDialog.Builder builder= new AlertDialog.Builder(EditFriendsActivity.this);
