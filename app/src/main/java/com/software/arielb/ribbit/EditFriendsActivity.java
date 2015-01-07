4dd7239e438a5dac82b75535b59ac263a2221068
Ariel <borochov@american.edu>
2015-01-07 00:11:27 -0500

    handeling backend

--- app/src/main/java/com/software/arielb/ribbit/EditFriendsActivity.java
+++ app/src/main/java/com/software/arielb/ribbit/EditFriendsActivity.java
@@ -7,13 +7,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.List;
 
@@ -21,16 +25,22 @@ import java.util.List;
 public class EditFriendsActivity extends ListActivity {
     public static final String TAG=EditFriendsActivity.class.getSimpleName();
     protected List<ParseUser> mUsers;
+    protected ParseRelation<ParseUser> mFriendsRelation;
+    protected ParseUser mCurrentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_edit_friends);
+
+        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
     }
     @Override
     protected void onResume(){
         super.onResume();
+        mCurrentUser=ParseUser.getCurrentUser();
+        mFriendsRelation=mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
         setProgressBarIndeterminateVisibility(true);
         ParseQuery<ParseUser> query= ParseUser.getQuery();
         query.orderByAscending(ParseConstants.KEY_USERNAME);
@@ -50,6 +60,7 @@ public class EditFriendsActivity extends ListActivity {
                     }
                     ArrayAdapter<String> adapter=new ArrayAdapter<String>(EditFriendsActivity.this,android.R.layout.simple_list_item_checked,usernames);
                     setListAdapter(adapter);
+                    addFriendCheckMarks();
                 }else {
                     Log.e(TAG, e.getMessage());
                     AlertDialog.Builder builder= new AlertDialog.Builder(EditFriendsActivity.this);
@@ -84,5 +95,47 @@ public class EditFriendsActivity extends ListActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        if(getListView().isItemChecked(position)){
+            //add friend
+            mFriendsRelation.add(mUsers.get(position));
+            mCurrentUser.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if(e!=null){
+                        Log.e(TAG,e.getMessage());
+                    }
+                }
+            });
+        }else{
+            //remove friend
+
+        }
+
+    }
+    private void addFriendCheckMarks(){
+        mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> friends, ParseException e) {
+                if(e==null){
+                    //success list returned
+                    for (int i=0;i<mUsers.size();i++){
+                        ParseUser user=mUsers.get(i);
+                        for (ParseUser friend :friends){
+                            if(friend.getObjectId().equals(user.getObjectId())){
+                                getListView().setItemChecked(i,true);
+                            }
+                        }
+                    }
+                }else {
+                    Log.e(TAG,e.getMessage());
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/software/arielb/ribbit/ParseConstants.java
+++ app/src/main/java/com/software/arielb/ribbit/ParseConstants.java
@@ -8,4 +8,5 @@ public final class ParseConstants {
 
     //field name
     public static final String KEY_USERNAME="username";
+    public static final String KEY_FRIENDS_RELATION="friendsRelation";
 }
